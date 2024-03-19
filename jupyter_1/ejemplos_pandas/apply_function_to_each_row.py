40a46dbbf3b6bf1f4c26a1dbc88a4a1f30feef7d
Mañanas <sgranjot@gmail.com>
2024-03-19 13:53:58 +0100

    more pandas examples added

---
+++ jupyter_1/ejemplos_pandas/apply_function_to_each_row.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from decimal import Decimal
+from io import StringIO
+
+"""
+Apply a function to each row of a DataFrame to create a new column
+
+"""
+
+
+def create_price_label(row):
+    price = Decimal(row['price'])
+    price = round(price, 2)
+    return row['name'].title() + " $" + str(price)
+
+
+fruit_price = r"""name,price
+banana,2.5
+grape,5.356
+apple,4.99
+"""
+
+df = pd.read_csv(StringIO(fruit_price))
+df['label'] = df.apply(create_price_label, axis=1)
+
+print(df)
