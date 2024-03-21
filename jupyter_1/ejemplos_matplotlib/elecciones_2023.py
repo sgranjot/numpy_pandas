341a73de546d9a5ff3256cd83166c4811a91aa4e
Sergio Granjo <sgranjot@gmail.com>
2024-03-21 22:17:43 +0100

    matplotlib elecciones generales 2023

--- jupyter_1/ejemplos_matplotlib/elecciones_2023.py
+++ jupyter_1/ejemplos_matplotlib/elecciones_2023.py
@@ -1,7 +1,19 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+
 df = pd.read_csv('elecciones_generales_2023.csv')
+
+df_pp = df[df['PARTIDO'] == 'PP']
+pp_por_distrito = df_pp.groupby('DISTRITO')['NUM_VOTOS'].sum()
+
+df_psoe = df[df['PARTIDO'] == 'PSOE']
+psoe_por_distrito = df_psoe.groupby('DISTRITO')['NUM_VOTOS'].sum()
+
+df_vox = df[df['PARTIDO'] == 'VOX']
+vox_por_distrito = df_vox.groupby('DISTRITO')['NUM_VOTOS'].sum()
 
 plt.figure(figsize=(14, 5))
 
@@ -9,25 +21,22 @@ plt.subplot(131)
 plt.title('Partido Popular')
 plt.xlabel('Distrito')
 plt.ylabel('Votos')
-df_PP = df['PARTIDO'] == 'PP'
-#df_PP_agrupado = df_PP.groupby('DISTRITO')['NUM_VOTOS'].sum().reset_index()
-plt.bar(df_PP['DISTRITO'], df_PP['NUM_VOTOS'], color='blue')
+pp_por_distrito.plot(kind='bar', color='blue')
+#plt.bar(pp_por_distrito['DISTRITO'], pp_por_distrito['NUM_VOTOS'], color='blue')
 
 plt.subplot(132)
 plt.title('PSOE')
 plt.xlabel('Distrito')
 plt.ylabel('Votos')
-df_PSOE = df['PARTIDO'] == 'PSOE'
-#df_PSOE_agrupado = df_PSOE.groupby('DISTRITO')['NUM_VOTOS'].sum().reset_index()
-plt.bar(df_PSOE['DISTRITO'], df_PSOE['NUM_VOTOS'], color='red')
+psoe_por_distrito.plot(kind='bar', color='red')
+#plt.bar(psoe_por_distrito['DISTRITO'], psoe_por_distrito['NUM_VOTOS'], color='red')
 
 plt.subplot(133)
 plt.title('VOX')
 plt.xlabel('Distrito')
 plt.ylabel('Votos')
-df_VOX = df['PARTIDO'] == 'PP'
-#df_VOX_agrupado = df_VOX.groupby('DISTRITO')['NUM_VOTOS'].sum().reset_index()
-plt.bar(df_VOX['DISTRITO'], df_VOX['NUM_VOTOS'], color='green')
+vox_por_distrito.plot(kind='bar', color='green')
+#plt.bar(vox_por_distrito['DISTRITO'], vox_por_distrito['NUM_VOTOS'], color='green')
 
 plt.suptitle('Principales partidos por distritos')
 plt.show()
